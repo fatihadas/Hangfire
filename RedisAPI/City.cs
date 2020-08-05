f402ad9299d7156a068492a349e63b3673b2d81d
Fatih ADAŞ <fatihadas93@gmail.com>
2020-08-06 00:08:36 +0300

    Add files via upload

---
+++ RedisAPI/City.cs
@@ -0,0 +1,8 @@
+﻿namespace RedisAPI
+{
+    public class City
+    {
+        public string CityId { get; set; }
+        public string CityName { get; set; }
+    }
+}
---
+++ RedisAPI/CityService.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace RedisAPI
+{
+    public class CityService
+    {
+        public static IEnumerable<City> GetCities(string countryId)
+        {
+            return new List<City>
+            {
+                new City() { CityId = "01", CityName = "Adana" },
+                new City() { CityId = "06", CityName = "Ankara" },
+                new City() { CityId = "34", CityName = "İstanbul" },
+                new City() { CityId = "35", CityName = "İzmir" }
+            };
+        }
+    }
+}
